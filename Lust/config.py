05a7f0c1ef9997fca850c8e27cc3487f7618f023
ＴｅａｍＳｈａｄｏｗｓ <alinwalker252@gmail.com>
2026-05-26 14:39:28 +0530

    Add files via upload

---
+++ Lust/config.py
@@ -0,0 +1,13 @@
+import os
+OWNER_ID = 7976292835
+GROUP_ID = "-1002236898779"
+TOKEN = "7769157535:AAH6OU7ew9bRnkY6_25I91byGR2EKfOA4pc"
+api_id = 35009179
+api_hash = "a76bbe9b7a26e7ad2af351b07dca34e2"
+PHOTO_URL = ["https://files.catbox.moe/fkxb0n.mp4"]
+SUPPORT_CHAT = "fetcher_X"
+UPDATE_CHAT = "fetcher_X"
+BOT_USERNAME = "AniLust_Bot"
+CHARA_CHANNEL_ID = -1003676047775
+MONGO_URL = "mongodb+srv://ra5836016_db_user:58360162004@cluster0.bqw1y2r.mongodb.net/?appName=Cluster0"
+DATABASE_NAME = "Lust_database"
